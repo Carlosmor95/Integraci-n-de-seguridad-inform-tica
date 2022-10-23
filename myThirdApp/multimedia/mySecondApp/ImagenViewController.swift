a081619620c98db76d9349121e4f8b983ab0dc27
Carlosmor95 <75329170+Carlosmor95@users.noreply.github.com>
2022-10-23 17:49:36 -0500

    Add files via upload

---
+++ myThirdApp/multimedia/mySecondApp/ImagenViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ImagenViewController.swift
+//  mySecondApp
+//
+//  Created by Alumno on 20/10/22.
+//
+
+import UIKit
+
+class ImagenViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
